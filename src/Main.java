1e779dfab9a9876a60bfe24fdf2613e3c99b2567
Your Name <you@example.com>
2025-04-24 14:56:14 +0200

    antekpedal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Gustaw", "Pedał");
+        Person person = new Person("Antek", "Pinglarz", 17);
 
         System.out.println(person);
     }
--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,12 @@
 public class Person {
     private String name;
     private String surname;
+    private int age;
 
-    public Person(String name, String surname) {
+    public Person(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
+        this.age = age;
     }
 
     public String getName() {
@@ -23,11 +25,21 @@ public class Person {
         this.surname = surname;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
+
